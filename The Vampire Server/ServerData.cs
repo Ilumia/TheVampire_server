0990b6fe89454bfd8f642dbfc500d611baef3898
Ilumia <oc56@naver.com>
2015-11-16 10:55:47 +0900

    Test ver. (hp:10, itemsetVer:3(입막음,금제 제외))

--- The Vampire Server/ServerData.cs
+++ The Vampire Server/ServerData.cs
@@ -111,7 +111,7 @@ namespace The_Vampire_Server
             this.state = ClientState.ONACCESS;
             item = new UserItem();
             SetAllItems();
-            hp = 5;
+            hp = 10;
             isHiding = false;
             isCamouflaging = false;
             isObserved = new List<Player>();
@@ -132,7 +132,7 @@ namespace The_Vampire_Server
             this.state = state;
             item = new UserItem();
             SetAllItems();
-            hp = 5;
+            hp = 10;
             isHiding = false;
             isCamouflaging = false;
             isObserved = new List<Player>();
